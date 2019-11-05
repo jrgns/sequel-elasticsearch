5f9e603ed498b905b7664867cc55efb7c08a1ef1
Jurgens du Toit <jrgns@jadeit.co.za>
2019-11-05 22:00:05 +0200

    refac: Rubocop improvements for spec

--- spec/sequel/plugins/elasticsearch_spec.rb
+++ spec/sequel/plugins/elasticsearch_spec.rb
@@ -1,7 +1,10 @@
+# frozen-string-literal: true
+
 require 'sequel'
 require 'sequel/plugins/elasticsearch'
 require 'sequel/plugins/elasticsearch/result'
 
+# rubocop: disable Metrics/BlockLength
 describe Sequel::Plugins::Elasticsearch do
   before(:all) do
     DB.create_table!(:documents) do
@@ -22,113 +25,113 @@ describe Sequel::Plugins::Elasticsearch do
     end
   end
 
-  let(:subject) do
+  let(:model) do
     Class.new(Sequel::Model(:documents))
   end
 
-  context '.configure' do
+  describe '.configure' do
     it 'defaults to the model table name for the index' do
-      subject.plugin :elasticsearch
-      expect(subject.send(:elasticsearch_index)).to eq :documents
+      model.plugin :elasticsearch
+      expect(model.send(:elasticsearch_index)).to eq :documents
     end
 
     it 'allows you to specify the index' do
-      subject.plugin :elasticsearch, index: :customIndex
-      expect(subject.elasticsearch_index).to eq :customIndex
+      model.plugin :elasticsearch, index: :customIndex
+      expect(model.elasticsearch_index).to eq :customIndex
     end
 
     it 'uses the specified index' do
-      subject.plugin :elasticsearch, index: :customIndex
+      model.plugin :elasticsearch, index: :customIndex
       stub_request(:put, %r{http://localhost:9200/customIndex/_doc/\d+})
-      doc = subject.new.save
+      doc = model.new.save
       expect(WebMock).to have_requested(:put, "http://localhost:9200/customIndex/_doc/#{doc.id}")
     end
 
     it 'defaults to `_doc` for the type' do
-      subject.plugin :elasticsearch
-      expect(subject.send(:elasticsearch_type)).to eq :_doc
+      model.plugin :elasticsearch
+      expect(model.send(:elasticsearch_type)).to eq :_doc
     end
 
     it 'allows you to specify the type' do
-      subject.plugin :elasticsearch, type: :customType
-      expect(subject.send(:elasticsearch_type)).to eq :customType
+      model.plugin :elasticsearch, type: :customType
+      expect(model.send(:elasticsearch_type)).to eq :customType
     end
 
     it 'uses the specified type' do
-      subject.plugin :elasticsearch, type: :customType
-      stub_request(:put, %r{http://localhost:9200/#{subject.table_name}/customType/\d+})
-      doc = subject.new.save
-      expect(WebMock).to have_requested(:put, "http://localhost:9200/#{subject.table_name}/customType/#{doc.id}")
+      model.plugin :elasticsearch, type: :customType
+      stub_request(:put, %r{http://localhost:9200/#{model.table_name}/customType/\d+})
+      doc = model.new.save
+      expect(WebMock).to have_requested(:put, "http://localhost:9200/#{model.table_name}/customType/#{doc.id}")
     end
 
     it 'allows you to pass down Elasticsearch client options' do
-      subject.plugin :elasticsearch, elasticsearch: { log: true }
-      expect(subject.new.es_client.transport.options).to include log: true
+      model.plugin :elasticsearch, elasticsearch: { log: true }
+      expect(model.new.es_client.transport.options).to include log: true
     end
   end
 
-  context 'ClassMethods' do
-    context '.es' do
+  describe 'ClassMethods' do
+    describe '.es' do
       it 'does a basic query string search' do
         stub_request(:get, %r{http://localhost:9200/documents/_doc/_search.*})
-        subject.plugin :elasticsearch
-        subject.es('test')
+        model.plugin :elasticsearch
+        model.es('test')
         expect(WebMock).to have_requested(:get, 'http://localhost:9200/documents/_doc/_search?q=test')
       end
 
       it 'does a complex query search' do
         stub = stub_request(:get, 'http://localhost:9200/documents/_doc/_search')
                .with(body: '{"query":{"match":{"title":"test"}}}')
-        subject.plugin :elasticsearch
-        subject.es(query: { match: { title: 'test' } })
+        model.plugin :elasticsearch
+        model.es(query: { match: { title: 'test' } })
         expect(stub).to have_been_requested.once
       end
 
       it 'handles not found exceptions' do
         stub_request(:get, %r{http://localhost:9200/documents/_doc/_search.*})
           .to_return(status: 404)
-        subject.plugin :elasticsearch
-        expect { subject.es('test') }.to_not raise_error
+        model.plugin :elasticsearch
+        expect { model.es('test') }.not_to raise_error
       end
 
       it 'handles connection failed exceptions' do
         stub_request(:get, %r{http://localhost:9200/documents/_doc/_search.*})
         allow(Faraday::Connection).to receive(:get).and_raise(Faraday::ConnectionFailed)
-        subject.plugin :elasticsearch
-        expect { subject.es('test') }.to_not raise_error
+        model.plugin :elasticsearch
+        expect { model.es('test') }.not_to raise_error
       end
 
       it 'returns an enumerable' do
         stub_request(:get, %r{http://localhost:9200/documents/_doc/_search.*})
-        subject.plugin :elasticsearch
-        expect(subject.es('test')).to be_a Enumerable
+        model.plugin :elasticsearch
+        expect(model.es('test')).to be_a Enumerable
       end
 
       it 'handles scroll requests' do
         stub = stub_request(:get, 'http://localhost:9200/documents/_doc/_search?q=test&scroll=1m')
-        subject.plugin :elasticsearch
-        subject.es('test', scroll: '1m')
+        model.plugin :elasticsearch
+        model.es('test', scroll: '1m')
         expect(stub).to have_been_requested.once
       end
 
       it 'handles scroll results'
     end
 
-    context '.es!' do
+    describe '.es!' do
       it 'does not handle exceptions' do
         stub_request(:get, %r{http://localhost:9200/documents/_doc/_search.*})
           .to_return(status: 500)
-        subject.plugin :elasticsearch
-        expect { subject.es!('test') }.to raise_error Elasticsearch::Transport::Transport::Error
+        model.plugin :elasticsearch
+        expect { model.es!('test') }.to raise_error Elasticsearch::Transport::Transport::Error
       end
     end
 
-    context '.scroll!' do
+    describe '.scroll!' do
       it 'accepts a scroll_id' do
         stub = stub_request(:get, 'http://localhost:9200/_search/scroll?scroll%5Bscroll%5D=1m&scroll_id=somescrollid')
                .to_return(status: 200)
-        subject.plugin :elasticsearch
-        subject.scroll!('somescrollid', scroll: '1m')
+        model.plugin :elasticsearch
+        model.scroll!('somescrollid', scroll: '1m')
         expect(stub).to have_been_requested.once
       end
 
@@ -137,21 +140,21 @@ describe Sequel::Plugins::Elasticsearch do
         allow(result).to receive(:scroll_id).and_return('somescrollid')
         stub = stub_request(:get, 'http://localhost:9200/_search/scroll?scroll%5Bscroll%5D=1m&scroll_id=somescrollid')
                .to_return(status: 200)
-        subject.plugin :elasticsearch
-        subject.scroll!(result, scroll: '1m')
+        model.plugin :elasticsearch
+        model.scroll!(result, scroll: '1m')
         expect(stub).to have_been_requested.once
       end
 
       it 'does not handle exceptions' do
         stub_request(:get, 'http://localhost:9200/_search/scroll?scroll=1m&scroll_id=somescrollid')
           .to_return(status: 500)
-        subject.plugin :elasticsearch
-        expect { subject.scroll!('somescrollid', '1m') }.to raise_error Elasticsearch::Transport::Transport::Error
+        model.plugin :elasticsearch
+        expect { model.scroll!('somescrollid', '1m') }.to raise_error Elasticsearch::Transport::Transport::Error
       end
     end
   end
 
-  context 'InstanceMethods' do
+  describe 'InstanceMethods' do
     let(:simple_doc) do
       @subj ||= begin
         subj = Class.new(Sequel::Model(:documents))
@@ -168,13 +171,13 @@ describe Sequel::Plugins::Elasticsearch do
       end
     end
 
-    context '#es_client' do
+    describe '#es_client' do
       it 'returns an Elasticsearch Transport Client' do
         expect(simple_doc.new.send(:es_client)).to be_a Elasticsearch::Transport::Client
       end
     end
 
-    context '#document_id' do
+    describe '#document_id' do
       it 'returns the value of the primary key for simple primary keys' do
         stub_request(:put, %r{http://localhost:9200/documents/_doc/\d+})
         doc = simple_doc.new.save
@@ -188,21 +191,20 @@ describe Sequel::Plugins::Elasticsearch do
       end
     end
 
-    context '#indexed_values' do
-      it 'correctly formats dates and other types' do
-        doc = simple_doc.new(
+    describe '#indexed_values' do
+      let(:doc) do
+        simple_doc.new(
           title: 'title',
           content: 'content',
           views: 4,
           active: true,
           created_at: Time.parse('2018-02-07T22:18:42+02:00')
         )
+      end
+
+      it 'correctly formats dates and other types' do
         expect(doc.as_indexed_json).to include(
-          title: 'title',
-          content: 'content',
-          views: 4,
-          active: true,
-          created_at: '2018-02-07T22:18:42+02:00'
+          title: 'title', content: 'content', views: 4, active: true, created_at: '2018-02-07T22:18:42+02:00'
         )
       end
 
@@ -215,7 +217,7 @@ describe Sequel::Plugins::Elasticsearch do
       end
     end
 
-    context '#document_path' do
+    describe '#document_path' do
       it 'returns the document index, type and id for documents' do
         stub_request(:put, %r{http://localhost:9200/documents/_doc/\d+})
         doc = simple_doc.new.save
@@ -225,7 +227,7 @@ describe Sequel::Plugins::Elasticsearch do
       end
     end
 
-    context '#save' do
+    describe '#save' do
       it 'indexes the document using the document path and model values' do
         stub_request(:put, %r{http://localhost:9200/documents/_doc/\d+})
         doc = simple_doc.new.save
@@ -234,28 +236,37 @@ describe Sequel::Plugins::Elasticsearch do
       end
     end
 
-    context '#update' do
-      it 'indexes the document using the document path and model values' do
-        stub_request(:put, %r{http://localhost:9200/documents/_doc/\d+})
+    describe '#update' do
+      let(:doc) do
         doc = simple_doc.new.save
         doc.title = 'updated'
         doc.save
+        doc
+      end
+
+      it 'indexes the document using the document path and model values' do
+        stub_request(:put, %r{http://localhost:9200/documents/_doc/\d+})
         expect(WebMock)
           .to have_requested(:put, "http://localhost:9200/#{simple_doc.table_name}/_doc/#{doc.id}")
           .times(2)
       end
     end
 
-    context '#destroy' do
-      it 'destroys the document using the document path' do
-        stub_request(:put, %r{http://localhost:9200/documents/_doc/\d+})
-        stub_request(:delete, %r{http://localhost:9200/documents/_doc/\d+})
+    describe '#destroy' do
+      let(:id) do
         doc = simple_doc.new.save
         id = doc.pk
         doc.destroy
+        id
+      end
+
+      it 'destroys the document using the document path' do
+        stub_request(:put, %r{http://localhost:9200/documents/_doc/\d+})
+        stub_request(:delete, %r{http://localhost:9200/documents/_doc/\d+})
         expect(WebMock)
           .to have_requested(:delete, "http://localhost:9200/#{simple_doc.table_name}/_doc/#{id}")
       end
     end
   end
 end
+# rubocop: enable Metrics/BlockLength
