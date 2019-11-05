7038f12af86cd9e54d570d7a7c1a48ff2388896e
Jurgens du Toit <jrgns@jadeit.co.za>
2019-11-05 22:39:46 +0200

    chore: Bump version

--- lib/sequel/plugins/elasticsearch/version.rb
+++ lib/sequel/plugins/elasticsearch/version.rb
@@ -4,6 +4,6 @@ module Sequel
   # See https://jrgns.github.io/sequel-elasticsearch
   module Elasticsearch
     # The Gem's version.
-    VERSION = '0.4.8'.freeze
+    VERSION = '0.4.9'.freeze
   end
 end
