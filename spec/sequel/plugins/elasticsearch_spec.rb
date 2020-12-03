1e502576be3443a043127c48fc33e86319d63fd0
Jurgens <jrgns@jadeit.co.za>
2020-12-03 12:36:07 +0200

    Revert "fix: Use new scroll API"
    This reverts commit 9b57958c4cdd44be72982443229099a67754dcf0.

--- spec/sequel/plugins/elasticsearch_spec.rb
+++ spec/sequel/plugins/elasticsearch_spec.rb
@@ -138,7 +138,7 @@ describe Sequel::Plugins::Elasticsearch do
       end
 
       it 'accepts a scroll_id' do
-        stub = stub_request(:get, 'http://localhost:9200/_search/scroll/somescrollid?scroll%5Bscroll%5D=1m')
+        stub = stub_request(:get, 'http://localhost:9200/_search/scroll?scroll%5Bscroll%5D=1m&scroll_id=somescrollid')
 
         model.scroll!('somescrollid', scroll: '1m')
         expect(stub).to have_been_requested.once
@@ -147,17 +147,18 @@ describe Sequel::Plugins::Elasticsearch do
       it 'accepts a Result' do
         result = Sequel::Plugins::Elasticsearch::Result.new('_scroll_id' => 'somescrollid')
         allow(result).to receive(:scroll_id).and_return('somescrollid')
-        stub = stub_request(:get, 'http://localhost:9200/_search/scroll/somescrollid?scroll%5Bscroll%5D=1m')
+        stub = stub_request(:get, 'http://localhost:9200/_search/scroll?scroll%5Bscroll%5D=1m&scroll_id=somescrollid')
                .to_return(status: 200)
 
         model.scroll!(result, scroll: '1m')
+
         expect(stub).to have_been_requested.once
       end
 
       it 'does not handle exceptions' do
-        stub_request(:get, 'http://localhost:9200/_search/scroll/somescrollid?scroll=1m')
+        stub_request(:get, 'http://localhost:9200/_search/scroll?scroll=1m&scroll_id=somescrollid')
           .to_return(status: 500)
-        expect { model.scroll!('somescrollid', '1m') }.to raise_error Elasticsearch::Transport::Transport::Error
+        expect { model.scroll!('somescrollid', '1m') }.to raise_error Elasticsearch::Transport::Transport::Error # Getting Faraday::ConnectionFailed ??
       end
     end
 
